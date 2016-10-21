2ba917e39c6023ab29d625e56322e7f6b6912a81
Tsai chen ping <oscar60310@gmail.com>
2016-10-21 19:39:11 +0800

    login and motion detect

--- XamarinAdvanceDemo/XamarinAdvanceDemo/Views/MainPage.xaml.cs
+++ XamarinAdvanceDemo/XamarinAdvanceDemo/Views/MainPage.xaml.cs
@@ -6,7 +6,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
 using Xamarin.Forms;
 
 namespace XamarinAdvanceDemo.Views
@@ -31,7 +34,7 @@ namespace XamarinAdvanceDemo.Views
             //use media plugin
             await CrossMedia.Current.Initialize();
             MediaFile photo;
-            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+          /*  if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported )
             {
                 photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
                 {
@@ -42,11 +45,54 @@ namespace XamarinAdvanceDemo.Views
             else
             {
                 photo = await CrossMedia.Current.PickPhotoAsync();
-            }
+
+            }*/
+            photo = await CrossMedia.Current.PickPhotoAsync();  //DEBUG
             if (photo != null)
             {
                 //UserDialogs.Instance.ShowSuccess("Success !");
-                CoverImage.Source = ImageSource.FromFile(photo.Path);                         
+                CoverImage.Source = ImageSource.FromFile(photo.Path);
+                try
+                {
+                    UserDialogs.Instance.ShowLoading("Login...");
+                
+                    //Login
+                    FaceServiceClient fc = new FaceServiceClient(Constant.FaceApiKey);
+                    var faceResult = await fc.DetectAsync(photo.GetStream());
+                    if (faceResult.Length != 1)
+                    {
+                        UserDialogs.Instance.HideLoading();
+                        UserDialogs.Instance.ShowError(faceResult.Length + " face detected.");
+                        return;
+                    }
+                    Guid[] ids = new Guid[1];
+                    ids[0] = faceResult[0].FaceId;
+
+                    var identyResult = (await fc.IdentifyAsync(Constant.DefaultGroupName, ids))[0].Candidates;
+
+                    if (identyResult.Length != 1)
+                    {
+                        UserDialogs.Instance.HideLoading();
+                        UserDialogs.Instance.ShowError("login failed.");
+                        return;
+                    }
+
+                    var personID = identyResult[0].PersonId;
+                    var persondetail = await fc.GetPersonAsync(Constant.DefaultGroupName, personID);
+                    UserDialogs.Instance.ShowLoading("Detecting emotion...");
+                    //Update Emotion database, should have only one face in photo
+                    EmotionServiceClient ec = new EmotionServiceClient(Constant.EmotionApiKey);
+                    Emotion[] emotionResult = await ec.RecognizeAsync(photo.GetStream());
+                    var emotionlisit = emotionResult[0].Scores.ToRankedList().ToList();
+
+                    UserDialogs.Instance.HideLoading();
+                    UserDialogs.Instance.ShowSuccess(persondetail.Name + "\n" + emotionlisit[0].Key);
+                }
+                catch(Exception ex)
+                {
+                    UserDialogs.Instance.HideLoading();
+                    UserDialogs.Instance.ShowError(ex.ToString());
+                }
             }
         }
     }
